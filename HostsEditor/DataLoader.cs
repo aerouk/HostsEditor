313c1a70a5330d049d047bf8c76d813d354c2897
Tom Brookes <me@iaero.me>
2016-12-06 18:14:27 +0000

    Add missing DataLoader file

---
+++ HostsEditor/DataLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace HostsEditor.Utils
+{
+    class DataLoader
+    {
+        public List<HostRow> hosts { get; set; }
+
+        public DataLoader()
+        {
+            hosts = new List<HostRow>();
+        }
+
+        public void LoadHostsFromFile()
+        {
+            string line;
+            int index = 0;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\windows\system32\drivers\etc\hosts");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                line = line.Trim();
+
+                if (line.Length > 0 && line.ToCharArray()[0] != '#')
+                {
+                    string[] lineContent = line.Split(new char[] { ' ', '\t' }, 2);
+
+                    hosts.Add(new HostRow(index, lineContent[0], lineContent[1]));
+                }
+
+                index++;
+            }
+
+            file.Close();
+        }
+    }
+}
